b4e805ff496cda43bcafb1eea0894f997eb92a09
manish-npx <mkdablu@gmail.com>
2025-11-12 14:58:21 +0530

    initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/manish-npx/simple-go-echo/internal/config"
+)
+
+func main() {
+
+	//config done
+	cfg := config.Config()
+
+	fmt.Printf("config value is %v", cfg)
+
+	//db connection
+
+	//routes
+}
